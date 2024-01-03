d6d935674491519c94f818cca0369ca53f963d63
headweek <abdullayev233@gmail.com>
2024-01-03 14:25:23 +0400

    Initial Commit

---
+++ m3hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m3hw
+//
+//  Created by Salman Abdullayev on 03.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
